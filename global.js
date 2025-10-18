975b2991bfa64f7102276104159bcd994a58e665
emcaii <emcai@ucsd.edu>
2025-10-17 20:08:33 -0700

    adding js nav

--- global.js
+++ global.js
@@ -3,3 +3,30 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "cv/", title: "CV" },
+  { url: "https://github.com/emcaii", title: "GitHub" }
+];
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/"; 
+
+let nav = document.createElement("nav");
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url.startsWith("http") ? p.url : BASE_PATH + p.url;
+  nav.insertAdjacentHTML("beforeend", `<a href="${url}">${p.title}</a>`);
+}
+
+let navLinks = $$("nav a");
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+currentLink?.classList.add("current");
