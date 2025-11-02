3abd8b208fafaf3b6425a2ec29fa7b0d25315591
emcaii <emcai@ucsd.edu>
2025-11-01 20:00:08 -0700

    add pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -11,3 +13,12 @@ const titleElement = document.querySelector('.projects-title');
 if (titleElement && Array.isArray(projects)) {
   titleElement.textContent = `Projects (${projects.length})`;
 }
+
+let svg = d3.select('#projects-pie-plot');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+svg
+  .append('path')
+  .attr('d', arcGenerator({ startAngle: 0, endAngle: 2 * Math.PI }))
+  .attr('fill', 'red');
