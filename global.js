ad8bc37a1847280fe730b7ff54fb3e371d073ad5
emcaii <emcai@ucsd.edu>
2025-11-08 21:46:03 -0800

    add meta

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ let pages = [
   { url: "projects/", title: "Projects" },
   { url: "contact/", title: "Contact" },
   { url: "cv/", title: "CV" },
+  { url: "meta/", title: "Meta"},
   { url: "https://github.com/emcaii", title: "GitHub" }
 ];
 
