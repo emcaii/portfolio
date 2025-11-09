8b9c73ef09b7b3d76f211bd444dc550a8e5f066f
emcaii <emcai@ucsd.edu>
2025-11-08 21:53:44 -0800

    steps until 2

--- meta/main.js
+++ meta/main.js
@@ -13,5 +13,59 @@ async function loadData() {
   return data;
 }
 
+function processCommits(data) {
+  return d3.groups(data, (d) => d.commit).map(([commit, lines]) => {
+    const first = lines[0];
+    const { author, date, time, timezone, datetime } = first;
+    const commitObj = {
+      id: commit,
+      url: 'https://github.com/YOUR_REPO/commit/' + commit,
+      author,
+      date,
+      time,
+      timezone,
+      datetime,
+      hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+      totalLines: lines.length,
+    };
+
+    // Add the full line data as a hidden property
+    Object.defineProperty(commitObj, 'lines', {
+      value: lines,
+      enumerable: false,  // hidden from console display
+      writable: false,
+      configurable: true,
+    });
+
+    return commitObj;
+  });
+}
+
 let data = await loadData();
-console.log(data);
+let commits = processCommits(data);
+
+console.log(commits);
+
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  // Number of distinct files
+  dl.append('dt').text('Files in codebase');
+  dl.append('dd').text(d3.group(data, d => d.file).size);
+
+  // Average line length
+  dl.append('dt').text('Average line length (chars)');
+  dl.append('dd').text(d3.mean(data, d => d.length).toFixed(1));
+
+  // Maximum depth
+  dl.append('dt').text('Maximum depth');
+  dl.append('dd').text(d3.max(data, d => d.depth));
+}
+
+renderCommitInfo(data, commits);
